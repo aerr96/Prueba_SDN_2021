d8911e5f1563cda15c5e84221096567dffb2402d
Alejandro Rivas <35722301+aerr96@users.noreply.github.com>
2021-03-24 18:03:35 -0400

    Update asignacion7.py

--- asignacion7.py
+++ asignacion7.py
@@ -4,5 +4,17 @@ from pprint import pprint
 response = requests.post(
     'https://sandboxdnac.cisco.com/dna/system/api/v1/auth/token',
     headers={'Authorization':'Basic ZGV2bmV0dXNlcjpDaXNjbzEyMyE='})
+response.raise_for_status()
 payload=response.json()
 pprint(payload)
+
+response = requests.get(
+    'https://sandboxdnac.cisco.com/dna/intent/api/v1/network-device',
+    headers={'X-Auth-Token':payload['Token']})
+list=response.json()['response']
+namelist=[]
+for j in range(len(list)):
+    namelist.append([list[j]['family'],list[j]['hostname'],
+    list[j]['managementIpAddress'],list[j]['lastUpdated'],
+    list[j]['reachabilityStatus']])
+pprint(namelist)
